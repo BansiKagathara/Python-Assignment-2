0ad9f93d300e6804ffef72ada34e6473d9c327d2
BansiKagathara <bansikagathara12@gmail.com>
2025-05-30 11:23:41 -0700

    Create smart home appliance.py

---
+++ smart home appliance.py
@@ -0,0 +1,20 @@
+#4. smart home appliance
+class Appliance:
+          def status(self):
+                 print("General appliance is working")
+
+class Fan(Appliance):
+          def status(self):
+                 print("Fan is running")
+
+class Light(Appliance):
+          def status(self):
+                  print("Light is on")
+
+class AC(Appliance):
+        def status(self):
+                print("AC is cooling")
+
+device=[Fan(),Light(),AC()]
+for device in device:
+      device.status()
