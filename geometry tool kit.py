e409aed0561932a801f81e9fde48f0d510e044cb
BansiKagathara <bansikagathara12@gmail.com>
2025-05-30 11:24:18 -0700

    Create geometry tool kit.py

---
+++ geometry tool kit.py
@@ -0,0 +1,12 @@
+#5. geometry tool kit
+import math
+class shapecalc:
+         def area(self,a,b=None):
+              if b is None:
+                     return math.pi*a*a
+              else:
+                     return a*b
+
+calc=shapecalc()
+print("Area of circle: ",calc.area(5))
+print("Area of rectangle: ",calc.area(4,6))
