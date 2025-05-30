77a22ff1bb6072bcb032078f3f3cb451603486b6
BansiKagathara <bansikagathara12@gmail.com>
2025-05-30 11:23:01 -0700

    Create Online Learning platform.py

---
+++ Online Learning platform.py
@@ -0,0 +1,44 @@
+#3.Online Learning platform
+
+class User:
+       def init(self,name,email):
+                self.name=name
+                self.email=email
+
+       def display_info(self):
+               print("User: ",self.name,"Email: ",self.email)
+
+class Instructor(User):
+       def init(self,name,email,subjects):
+              super().init(name,email)
+              self.subjects=subjects
+
+       def upload_content(self):
+              print(self.name,"upload content on",self.subjects)
+
+       def display_info(self):
+              print("instructor:",self.name,"Subject: ",self.subjects)
+              
+class CourseCreator(Instructor):
+       def init(self,name,email,subjects,modules):
+              super().init(name,email,subjects)
+              self.modules=modules
+       def create(self):
+              print("Creating course")
+              for module in self.modules:
+                     print("-",module)
+
+       def display_info(self):
+              print("CourseCreator: ",self.name,"Modules: ",self.modules)
+       
+user = User("Anita", "anita@mail.com")
+user.display_info()
+
+instructor = Instructor("Raj", "raj@mail.com", "Math")
+instructor.display_info()
+instructor.upload_content()
+
+creator = CourseCreator("Kiran", "kiran@mail.com", "Science", ["Module1", "Module2"])
+creator.display_info()
+creator.create()
+
